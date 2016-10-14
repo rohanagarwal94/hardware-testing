8e5fb3c88f0fa2786b25195bd2711c21b85422ce
Abhishek Yadav <abhishekgit1208@gmail.com>
2016-10-14 21:37:08 +0530

    On item click listener added

--- app/src/main/java/com/example/hardware/MainActivity.java
+++ app/src/main/java/com/example/hardware/MainActivity.java
@@ -32,10 +32,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         lv = (RecyclerView) findViewById(R.id.listView1);
-       ListViewAdapter listViewAdapter=new ListViewAdapter();
-        LinearLayoutManager lm=new LinearLayoutManager(this);
+        ListViewAdapter listViewAdapter = new ListViewAdapter();
+        LinearLayoutManager lm = new LinearLayoutManager(this);
         lv.setLayoutManager(lm);
         lv.setAdapter(listViewAdapter);
+
 
 //            lv.setAdapter(adapt);
 //	        lv.setOnItemClickListener(this);
@@ -149,27 +150,139 @@ public class MainActivity extends Activity {
 //
 
     }
-    public class ListViewHolder extends RecyclerView.ViewHolder{
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        View v;
 
         public ListViewHolder(View itemView) {
             super(itemView);
+            this.v = itemView;
         }
+
         TextView category;
     }
-    public  class ListViewAdapter extends RecyclerView.Adapter<ListViewHolder>{
+
+    public class ListViewAdapter extends RecyclerView.Adapter<ListViewHolder> {
 
         @Override
         public ListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View contextView=getLayoutInflater().inflate(R.layout.extraa,parent,false);
-            ListViewHolder listViewHolder=new ListViewHolder(contextView);
-            listViewHolder.category= (TextView) contextView.findViewById(R.id.tv_extraa);
+            View contextView = getLayoutInflater().inflate(R.layout.extraa, parent, false);
+            ListViewHolder listViewHolder = new ListViewHolder(contextView);
+            listViewHolder.category = (TextView) contextView.findViewById(R.id.tv_extraa);
 
             return listViewHolder;
         }
 
         @Override
-        public void onBindViewHolder(ListViewHolder holder, int position) {
-        holder.category.setText(items[position]);
+        public void onBindViewHolder(ListViewHolder holder, final int position) {
+            holder.category.setText(items[position]);
+            holder.v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (position) {
+                        case 0:
+                            //not working
+                            intent = new Intent(MainActivity.this, Vibration.class);
+                            startActivity(intent);
+                            break;
+                        case 2:
+                            intent = new Intent(MainActivity.this, Tele.class);
+                            startActivity(intent);
+                            break;
+                        case 1:
+                            //not whole info
+                            intent = new Intent(getApplicationContext(), Systeminfo.class);
+                            startActivity(intent);
+                            break;
+                        case 3:
+
+                            intent = new Intent(getApplicationContext(), ProximitySensor.class);
+                            startActivity(intent);
+                            break;
+
+                        case 4:
+                            //not working
+                            intent = new Intent(getApplicationContext(), Flash.class);
+                            startActivity(intent);
+                            break;
+
+                        case 5:
+
+                            intent = new Intent(getApplicationContext(), TouchSensor.class);
+                            startActivity(intent);
+                            break;
+                        case 6:
+                            //display multiple lighting
+                            intent = new Intent(getApplicationContext(), Display.class);
+                            startActivity(intent);
+                            break;
+                        case 7:
+                            //light sensor display of dac values
+                            intent = new Intent(getApplicationContext(), Lightsensor.class);
+                            startActivity(intent);
+                            break;
+                        case 8:
+
+                            intent = new Intent(getApplicationContext(), Pressure.class);
+                            startActivity(intent);
+                            break;
+
+                        case 9:
+                            //wrong the volume buttons,home,menu etc has to be checked here
+                            intent = new Intent(getApplicationContext(), Buttontesting.class);
+                            startActivity(intent);
+                            break;
+                        case 10:
+                            //wrong should have done that while taking calling position we here our voice
+                            intent = new Intent(getApplicationContext(), Mictesting.class);
+                            startActivity(intent);
+                            break;
+                        case 11:
+
+                            intent = new Intent(getApplicationContext(), Wifiaddress.class);
+                            startActivity(intent);
+                            break;
+                        case 12:
+
+                            intent = new Intent(getApplicationContext(), Blueadd.class);
+                            startActivity(intent);
+                            break;
+                        case 13:
+
+                            intent = new Intent(getApplicationContext(), Gravitysensor.class);
+                            startActivity(intent);
+                            break;
+
+                        case 14:
+                            intent = new Intent(getApplicationContext(), Magneticsensor.class);
+                            startActivity(intent);
+                            break;
+                        case 15:
+                            //not working
+                            intent = new Intent(getApplicationContext(), Headphone.class);
+                            startActivity(intent);
+                            break;
+                        case 16:
+                            intent = new Intent(getApplicationContext(), Gyroscope.class);
+                            startActivity(intent);
+                            break;
+                        case 17:
+                            //test again
+                            intent = new Intent(getApplicationContext(), Gpsloc.class);
+                            startActivity(intent);
+                            break;
+                        case 18:
+                            intent = new Intent(getApplicationContext(), Batteryindicator.class);
+                            startActivity(intent);
+                            break;
+                        case 19:
+                            intent = new Intent(getApplicationContext(), Accelarometer.class);
+                            startActivity(intent);
+                            break;
+                    }
+
+                }
+            });
         }
 
         @Override
@@ -177,5 +290,6 @@ public class MainActivity extends Activity {
             return items.length;
         }
     }
+
 
 }
